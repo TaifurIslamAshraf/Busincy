6be096cb65496c686fe45779e319e032ebd30f35
Taifur <taifurislamashraf@gmail.com>
2023-12-22 21:14:55 +0600

    design hero section

--- js/main.js
+++ js/main.js
@@ -3,6 +3,16 @@ import "aos/dist/aos.css";
 
 //Init Aos Animations
 Aos.init({
-    duration: 500,
+    duration: 1000,
     offset: 100
+})
+
+
+//header active mobile
+const headerMobile = document.getElementById("mobile_menu")
+const homeNav = document.querySelector(".home-nav")
+
+headerMobile.addEventListener("click", function(){
+    homeNav.classList.toggle("active-mobile")
+    headerMobile.classList.toggle("active-fa-bars")
 })
