0579368625229e254f6724d204547a4b7744bfa5
Taifur <taifurislamashraf@gmail.com>
2024-01-04 19:06:28 +0600

    some fix and play button design with animations

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 import { homeHeaderMobileActive, teamSwiperSlider, testimonialsSwiperSlider } from "./main";
 
 
+// play button texts
+const playText = document.querySelector(".hero_play_btn-text p")
+const arrayOfText = playText.innerHTML.split("").map((char,i)=> (
+    `<span style="transform:rotate(${i * 16.4}deg)" >${char}</span>`
+)).join("")
+playText.innerHTML = arrayOfText
+
 
 homeHeaderMobileActive();
 teamSwiperSlider()
